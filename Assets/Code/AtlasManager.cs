1465885ca26d820ca025de71b8edc6cf7d3208e7
harald921@hotmail.com <harald921@hotmail.com>
2017-10-30 02:29:28 +0100

    I am going to die if this doesn't push

---
+++ Assets/Code/AtlasManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtlasManager : MonoBehaviour
+{
+    [SerializeField] Texture2D _atlas;
+    public Texture2D atlas
+    {
+        get
+        {
+            return _atlas;
+        }
+    }
+}
