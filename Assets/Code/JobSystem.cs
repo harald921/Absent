21ec93628f23cf44c627cb3ec562ababf3e851c1
Harald <harald921@hotmail.com>
2017-11-04 12:49:15 +0100

    Minor renaming in JobSystem.cs

--- Assets/Code/JobSystem.cs
+++ Assets/Code/JobSystem.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 public class JobSystem : MonoBehaviour
 {
     // Singleton
-    public static JobSystem _instance;
+    static JobSystem _instance;
     public static JobSystem instance
     {
         get
@@ -26,14 +26,14 @@ public class JobSystem : MonoBehaviour
     // Job class
     class Job
     {
-        public Func<object> methodToThread;   // The method that needs to be threaded. It returns an object.
-        public Action<object> onJobFinish;    // The callback that should run when the work is complete. It takes an object as a parameter.
+        public Func<object> methodToThread;         // The method that needs to be threaded. It returns an object.
+        public Action<object> mainThreadCallback;   // The callback that should run when the work is complete. It takes an object as a parameter.
 
-        public Thread jobThread;              // The thread which the threaded method is run on
+        public Thread jobThread;                    // The thread which the threaded method is run on
 
-        public object result;                 // The result of the thread. This will be set whenever the job is done.
+        public object result;                       // The result of the thread. This will be set whenever the job is done.
 
-        public void Start()
+        public void Begin()
         {
             result = methodToThread();
         }
@@ -48,19 +48,20 @@ public class JobSystem : MonoBehaviour
         for (int i = currentJobs.Count - 1; i >= 0; i--)
             if (!currentJobs[i].jobThread.IsAlive)
             {
-                currentJobs[i].onJobFinish(currentJobs[i].result);
+                currentJobs[i].mainThreadCallback(currentJobs[i].result);
                 currentJobs.RemoveAt(i);
             }
     }
 
-    public void DoThreaded(Func<object> inMethodToThread, Action<object> inOnJobFinish)
+    public void DoThreaded(Func<object> inMethodToThread, Action<object> inMainThreadCallback)
     {
         Job newJob = new Job();
-        newJob.methodToThread = inMethodToThread;
-        newJob.onJobFinish = inOnJobFinish;
-        newJob.jobThread = new Thread(() => newJob.Start());
 
         currentJobs.Add(newJob);
+
+        newJob.methodToThread = inMethodToThread;
+        newJob.mainThreadCallback    = inMainThreadCallback;
+        newJob.jobThread      = new Thread(() => newJob.Begin());
 
         newJob.jobThread.Start();
     }
